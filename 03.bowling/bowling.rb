919e5ad193ca27f03a3bfc40a5815bf6f28fcc43
nkhkcat <7u6a6i@gmail.com>
2023-10-03 23:53:56 +0900

    RuboCopのエラーを解消するための修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 score = ARGV[0]
 scores = score.split(',')
@@ -13,7 +14,6 @@ scores.each do |s|
 end
 
 # 倒したピンの数のみを合計する
-sum_point = 0
 sum_point = shots.sum
 
 frames = []
@@ -25,17 +25,19 @@ end
 add_strike_point = 0
 add_spare_point = 0
 
-number = 8 # ストライクかスペアによる加算の対象である9フレーム目まで配列を回すための数
-for i in 0..number do
-	if frames[i][0] == 10 # ストライクの場合
-		if frames[i+1][0] == 10 # 2連続ストライクの場合
-			add_strike_point += frames[i+1][0] + frames[i+2][0]
-		else
-			add_strike_point += frames[i+1].sum
-		end
-	elsif frames[i].sum == 10 # スペアの場合
-		add_spare_point += frames[i+1][0]
-	end
+frames.each_with_index do |frame, i|
+  break if i == 9
+
+  if frame[0] == 10 # ストライクの場合
+    if frames[i + 1][0] == 10 # 2連続ストライクの場合
+      add_strike_point += frames[i + 1][0]
+      add_strike_point += frames[i + 2][0]
+    else
+      add_strike_point += frames[i + 1].sum
+    end
+  elsif frame.sum == 10 # スペアの場合
+    add_spare_point += frames[i + 1][0]
+  end
 end
 
 total_point = sum_point + add_strike_point + add_spare_point
