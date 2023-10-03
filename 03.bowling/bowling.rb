c3ce054a94feff03f3a7b4f50c610644da452678
nkhkcat <7u6a6i@gmail.com>
2023-10-03 22:28:19 +0900

    新ルールに対応させる変更

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -12,19 +12,31 @@ scores.each do |s|
   end
 end
 
+# 倒したピンの数のみを合計する
+sum_point = 0
+sum_point = shots.sum
+
 frames = []
 shots.each_slice(2) do |s|
   frames << s
 end
 
-point = 0
-frames.each do |frame|
-  if frame[0] == 10 # strike
-    point += 30
-  elsif frame.sum == 10 # spare
-    point += frame[0] + 10
-  else
-    point += frame.sum
-  end
+# ストライクとスペアによる加算をする
+add_strike_point = 0
+add_spare_point = 0
+
+number = 8 # ストライクかスペアによる加算の対象である9フレーム目まで配列を回すための数
+for i in 0..number do
+	if frames[i][0] == 10 # ストライクの場合
+		if frames[i+1][0] == 10 # 2連続ストライクの場合
+			add_strike_point += frames[i+1][0] + frames[i+2][0]
+		else
+			add_strike_point += frames[i+1].sum
+		end
+	elsif frames[i].sum == 10 # スペアの場合
+		add_spare_point += frames[i+1][0]
+	end
 end
-puts point
+
+total_point = sum_point + add_strike_point + add_spare_point
+puts total_point
