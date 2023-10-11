55c20bdfda1382e73a830e98370963d1e94f40c5
nkhkcat <7u6a6i@gmail.com>
2023-10-11 09:33:21 +0900

    変数名を動詞から始めるとメソッドに見えるので名詞から始めるように変更

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -22,23 +22,23 @@ shots.each_slice(2) do |s|
 end
 
 # ストライクとスペアによる加算をする
-add_strike_point = 0
-add_spare_point = 0
+strike_point = 0
+spare_point = 0
 
 frames.each_with_index do |frame, i|
   break if i == 9
 
   if frame[0] == 10 # ストライクの場合
     if frames[i + 1][0] == 10 # 2連続ストライクの場合
-      add_strike_point += frames[i + 1][0]
-      add_strike_point += frames[i + 2][0]
+      strike_point += frames[i + 1][0]
+      strike_point += frames[i + 2][0]
     else
-      add_strike_point += frames[i + 1].sum
+      strike_point += frames[i + 1].sum
     end
   elsif frame.sum == 10 # スペアの場合
-    add_spare_point += frames[i + 1][0]
+    spare_point += frames[i + 1][0]
   end
 end
 
-total_point = sum_point + add_strike_point + add_spare_point
+total_point = sum_point + strike_point + spare_point
 puts total_point
