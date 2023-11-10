cc6d3c5ab54769969bb09e31a5c9a45067ba276a
nkhkcat <7u6a6i@gmail.com>
2023-11-10 12:47:41 +0900

    リファクタリング：まとめるべきでない処理をメソッドにまとめるのをやめた

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -6,38 +6,10 @@ require 'optparse'
 COLUMNS_NUMBER = 3
 
 def main
-  show_file_names(file_names)
-end
-
-def file_names
   options = {}
   OptionParser.new { |opt| opt.on('-a') { options[:show_all] = true } }.parse!(ARGV)
-  path = ARGV.empty? ? '.' : ARGV[0]
-  if !Dir.exist?(path) && !File.exist?(path)
-    puts "ls: #{ARGV[0]}: No such file or directory"
-    exit(1)
-  end
 
-  if options[:show_all]
-    Dir.entries(path).sort
-  else
-    Dir.entries(path).delete_if { |file_name| file_name.start_with?('.') }.sort
-  end
-end
-
-def split_array_vertically(input_array, columns_number)
-  return [] if input_array.empty? || columns_number.zero?
-
-  elements_number = (input_array.size / columns_number.to_f).ceil
-  result = Array.new(elements_number) { [] }
-  input_array.each_with_index do |item, index|
-    target_group = index % elements_number
-    result[target_group] << item
-  end
-  result
-end
-
-def show_file_names(file_name_list)
+  file_name_list = Dir.entries(path).sort.delete_if { |file_name| !options[:show_all] && file_name.start_with?('.') }
   max_file_name_length = 0
   file_name_list.each do |file|
     max_file_name_length = file.length if file.length > max_file_name_length
@@ -52,6 +24,27 @@ def show_file_names(file_name_list)
     end
     puts
   end
+end
+
+def path
+  path = ARGV.empty? ? '.' : ARGV[0]
+  if !Dir.exist?(path) && !File.exist?(path)
+    puts "ls: #{ARGV[0]}: No such file or directory"
+    exit(1)
+  end
+  path
+end
+
+def split_array_vertically(input_array, columns_number)
+  return [] if input_array.empty? || columns_number.zero?
+
+  elements_number = (input_array.size / columns_number.to_f).ceil
+  result = Array.new(elements_number) { [] }
+  input_array.each_with_index do |item, index|
+    target_group = index % elements_number
+    result[target_group] << item
+  end
+  result
 end
 
 main
