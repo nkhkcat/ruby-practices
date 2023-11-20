83b29766cbf5b9fe09070c625acbefa53dd66223
nkhkcat <7u6a6i@gmail.com>
2023-11-20 18:39:47 +0900

    新規追加：-lオプションの処理を追加した

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,6 +2,8 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'fileutils'
+require 'etc'
 
 COLUMNS_NUMBER = 3
 
@@ -10,10 +12,20 @@ def main
   OptionParser.new do |opt|
     opt.on('-a') { options[:show_all] = true }
     opt.on('-r') { options[:reverse] = true }
+    opt.on('-l') { options[:long_format] = true }
   end.parse!(ARGV)
 
   files = Dir.entries(path).sort.delete_if { |file_name| !options[:show_all] && file_name.start_with?('.') }
   files.reverse! if options[:reverse]
+  if options[:long_format]
+    puts "total #{total_blocks(path, options[:show_all])}"
+    files.each do |file|
+      file_path = "#{path}/#{file}"
+      show_file_info(file_path)
+    end
+    exit 0
+  end
+
   max_file_name_length = 0
   files.each do |file|
     max_file_name_length = file.length if file.length > max_file_name_length
@@ -49,6 +61,39 @@ def split_array_vertically(input_array, columns_number)
     result[target_group] << item
   end
   result
+end
+
+def show_file_info(path)
+  file_stat = File.stat(path)
+  permissions = permissions_to_s(file_stat.mode)
+  hard_link_number = "%2d" % file_stat.nlink
+  owner_name = Etc.getpwuid(file_stat.uid).name
+  group_name = "%3s" % Etc.getgrgid(file_stat.gid).name
+  file_size = "%6d" % file_stat.size
+  last_modified = file_stat.mtime.strftime('%m %e %H:%M')
+  file_name = File.basename(path)
+
+  puts "#{permissions} #{hard_link_number} #{owner_name} #{group_name} #{file_size} #{last_modified} #{file_name}"
+end
+
+def permissions_to_s(mode)
+  type = mode[0] == 0 ? '-' : 'd'
+
+  perms = [String.new('---'), String.new('---'), String.new('---')]
+  [['r', 256], ['w', 128], ['x', 64], ['r', 32], ['w', 16], ['x', 8], ['r', 4], ['w', 2], ['x', 1]].each_with_index do |(char, mask), i|
+    perms[i / 3][i % 3] = char if mode & mask != 0
+  end
+
+  "#{type}#{perms.join}"
+end
+
+def total_blocks(directory, show_all)
+  total = 0
+  Dir.foreach(directory) do |file|
+    next if file == '.' || file == '..'
+    total += File.stat(File.join(directory, file)).blocks if show_all || !file.start_with?('.')
+  end
+  total
 end
 
 main
